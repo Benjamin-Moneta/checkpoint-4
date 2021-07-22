70fff28abead2b7b74aa4d0d3279b84ba1d06a9b
Benjamin-Moneta <benjamin.moneta@orange.fr>
2021-07-22 11:44:52 +0200

    Galerie dynamique fonctionnel

--- src/Helpers/CarouselData.jsx
+++ src/Helpers/CarouselData.jsx
@@ -1,9 +1,18 @@
-import City from "../../img/voiture.jpg";
-import San from "../../img/photoRM.jpg";
-import Diago from "../../img/jadikan.jpg";
+import Appareil from "../../img/voiture.jpg";
+import Cercle from "../../img/photoRM.jpg";
+import Entrepot from "../../img/jadikan.jpg";
+import Fleurs from "../../img/fleurs.jpg";
+import Bulle from "../../img/bulle.jpeg";
+import Banc from "../../img/banc.jpg";
+import Spiral from "../../img/spiral.jpg";
 
 export const images = [
-  { title: "San Diego", subtitle: "This is San Diago", img: City },
-  { title: "San", subtitle: "This is San", img: San },
-  { title: "Diego", subtitle: "This is Diago", img: Diago },
+  { title: "Appareil", subtitle: "Appareil photo lumineux", img: Appareil },
+  { title: "Cercle", subtitle: "Cercle lumineux", img: Cercle },
+  { title: "Entrepot", subtitle: "L'entrepôt de lumière", img: Entrepot },
+  { title: "Fleurs", subtitle: "La fleur de lumière", img: Fleurs },
+  { title: "Bulle", subtitle: "La bulle en lumière", img: Bulle },
+  { title: "Banc", subtitle: "L'homme du banc", img: Banc },
+  { title: "Spiral", subtitle: "La spiral de lumière", img: Spiral },
+
 ];
