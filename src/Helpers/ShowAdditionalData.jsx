3ff8e079a6904fc4d9721b697d43453f82e8d868
Benjamin-Moneta <benjamin.moneta@orange.fr>
2021-07-23 11:14:46 +0200

    Page ressources terminer

--- src/Helpers/ShowAdditionalData.jsx
+++ src/Helpers/ShowAdditionalData.jsx
@@ -13,12 +13,33 @@ import Expo4 from "../../img/lieuexpo6.jpeg";
 
 export const data = [
   {
-    title1: "Exposition Lumières dans l’Espace à Enghien (95)",
+    title: "Exposition Lumières dans l’Espace à Enghien (95)",
     date: "du 13 octobre au 24 octobre",
-    avatar1: Fleurs,
-    description1:
+    avatar: Fleurs,
+    description:
       "Première exposition en région parisienne de la Ligue Francophone de Light Painting – LFLP – au Centre culturel François-Villon.",
-    paragraphe1:
+    paragraphe:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: Expo1,
+  },
+  {
+    title: "Exposition Peinture et Lumières à Vedène (84) au Château de Fargues",
+    date: "du 27 octobre au décembre",
+    avatar: Entrepot,
+    description:
+      "Padawan vous propose de découvrir son art, le light painting. Un lieu sombre, un appareil photo, des pinceaux lumineux, il n'en faut pas plus à ce peintre en lumières pour se mettre à l’œuvre.",
+    paragraphe:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: Expo2,
+  },
+  {
+    title: "Exposition Chorégraphies nocturnes à Cannes (06)",
+    date: "du 27 décembre au 16 janvier",
+    avatar: Banc,
+    description:
+      "Jadikan sort de l'ombre les friches industrielles. L’artiste Isérois a trouvé des décors à la hauteur de son imagination en se rendant sur les centrales hydroélectriques laissés à l’abandon.",
+    paragraphe:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: Expo3,
   },
 ];
--- src/components/Ressources.jsx
+++ src/components/Ressources.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState }from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import clsx from "clsx";
 import Card from "@material-ui/core/Card";
@@ -15,11 +15,13 @@ import FavoriteIcon from "@material-ui/icons/Favorite";
 import ShareIcon from "@material-ui/icons/Share";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
-// import { data } from "../Helpers/CarouselData";
+import { data } from "../Helpers/ShowAdditionalData";
+import "./Ressources.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 500,
+    marginBottom: 130,
   },
   media: {
     height: 0,
@@ -42,6 +44,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Ressources() {
   const classes = useStyles();
+  const [cards, setCards] = useState(0);
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
@@ -49,11 +52,12 @@ export default function Ressources() {
   };
 
   return (
-    <Card className={classes.root}>
+    <>
+    {data.map(card => <Card className={classes.root}>
       <CardHeader
         avatar={
           <Avatar aria-label="recipe" className={classes.avatar}>
-            <img className="imgRessources" src="../../img/voiture.jpg"></img>
+            <img className="imgRessources" src={card.avatar}/>
           </Avatar>
         }
         action={
@@ -61,17 +65,17 @@ export default function Ressources() {
             <MoreVertIcon />
           </IconButton>
         }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
+        title={card.title}
+        subheader={card.date}
       />
       <CardMedia
         className={classes.media}
-        image="/static/images/cards/paella.jpg"
-        title="Paella dish"
+        image={card.img}
+        title={card.date}
       />
       <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
-           thanos
+           <p className="description1">{card.description}</p>
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
@@ -94,35 +98,12 @@ export default function Ressources() {
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
           <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and
-            set aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-            over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-            stirring occasionally until lightly browned, 6 to 8 minutes.
-            Transfer shrimp to a large plate and set aside, leaving chicken and
-            chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes,
-            onion, salt and pepper, and cook, stirring often until thickened and
-            fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
-            cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is
-            absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved
-            shrimp and mussels, tucking them down into the rice, and cook again
-            without stirring, until mussels have opened and rice is just tender,
-            5 to 7 minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then
-            serve.
+            <p className="paragraphe1">{card.paragraphe}</p>
           </Typography>
         </CardContent>
       </Collapse>
-    </Card>
+    </Card>)}
+    </>
   );
 }
